e2217484e5d1d94331340c1c33cbf7976476b62e
Mortec <contact@edouardmortec.com>
2020-11-13 05:37:49 +0100

    Picture store & PictureView implementation

--- src/components/PictureView.svelte
+++ src/components/PictureView.svelte
@@ -1,17 +1,38 @@
 <script>
+
+
   import { onMount } from "svelte"
   import { picture } from "../stores/Picture.js"
 
   export let src = "./assets/images/Michael-Faraday.jpg"
 
-  let width, height
+  const id = "pictureCanvas"
+
+  let pwidth, pheight
 
   onMount(() => { 
-    picture.load(src);
-    picture.tune({blur: 0.3})
-    picture.draw()
+    picture.init(src, id);
+  
+    picture.subscribe( s =>{
+
+
+      // s.ctxt.scale(s.zoom, s.zoom)
+      s.ctxt.filter = `brightness(${s.brightness}) contrast(${s.contrast}) saturate(${s.saturation}) blur(${s.blur*5}px)`
+      s.image.onload = () => {
+        
+        s.ctxt.canvas.width = s.width
+        s.ctxt.canvas.height = s.height
+
+        const offset = {
+          x: Math.floor( (s.image.width - s.width/s.zoom)/2 ),
+          y: Math.floor( (s.image.height - s.height/s.zoom)/2 )
+        }
+        s.ctxt.drawImage(s.image, offset.x, offset.y, s.width/s.zoom, s.height/s.zoom, 0, 0, s.width, s.height )
+      }
+    })
   })
 
+  $: picture.tune( {width: pwidth, height: pheight})
 
 </script>
 
@@ -25,15 +46,16 @@
   }
 
   canvas {
-    width: 100%;
-    height: 100%;
-    padding: none;
+      /* width: 216px;
+      height: 279px; */
+    padding: 0px;
+    margin: 0px;
     background-color: whitesmoke;
   }
 </style>
 
 <!-- HTML -------------------------------------------------------- -->
 
-<div class="picture" bind:clientWidth={width} bind:clientHeight={height}>
-  <canvas id="pictureCanvas" />
+<div class="picture" bind:clientWidth={pwidth} bind:clientHeight={pheight}>
+  <canvas {id} {pwidth} {pheight} />
 </div>
