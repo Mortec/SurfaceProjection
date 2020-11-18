1f806332b158b3d853fcfc2c137ff9051d0e2643
Mortec <contact@edouardmortec.com>
2020-11-18 03:59:09 +0100

    more main layout

--- src/App.svelte
+++ src/App.svelte
@@ -9,116 +9,113 @@
 <!-- STYLE -------------------------------------------------------- -->
 
 <style>
-.container{
-	display: flex;
-	flex-direction: row;
-	width: 100%;
-	height: 100%;
-}
 
-
-	nav {
-	
+	.container{
+		width: 100%;
+		height: 100%;
 	}
-	.navbutton {
-		height: 10%;
-		vertical-align: center;
+
+	header {
+		margin: none;
+		padding-top: 1em;
+		padding-left: 3em;
+		height: 8vh;
+	}
+	
+	h1 {
+	  font-size: 1.5em;
+	  margin: none;
+	  padding:none;
 	}
 
 	main{
 		width: 100%;
+		height: 83vh;
 		display: flex;
-		flex-direction: column;
-		justify-content: space-between;
+		flex-direction: row;
+		justify-content: flex-start;
+
 	}
-
-	header {
-	display: flex;
-	flex-direction: row;
-	align-items: center;
-	justify-content: flex-start;
-	height: 10%;
-	margin: none;
-	padding: none;
-	padding-left: 1.5em;
-	}
-
-  h1 {
-    font-size: 1.5em;
-    margin: none;
-  }
-
-  .playground {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    align-items: flex-start;
-    margin-top: 1em;
-	margin-bottom: 1em;
-	padding-left: 3em;
-	height: auto;
 	
-  }
-  footer {
+	nav {
+		display: block;
+		width: 3vw;
+		margin: none;
+		padding: none;
+	}
+
+
+	.playground {
+		display: flex;
+		flex-direction: row;
+		align-items: flex-start;
+		justify-content: space-around;
+		padding-top: 1em;
+		padding-left: 1em;
+
+		height: 100%;
+		width: 100%
+		
+	}
+
+  	footer {
 		display: flex;
 		flex-direction: row;
 		align-items: center;
 		justify-content: flex-end;
-		height: 10%;
+		height: auto;
 		padding-right: 2em;
 	}
 
 
 	.gcode {
-	display: flex;
-	flex-direction: row;
-	align-items: center;
-	justify-self: flex-start;
-	align-self: flex-start;
-	margin: none;
-	padding: none;
-  }
-  input[type="text"] {
-    font-family: "Cutive Mono", monospace;
-    letter-spacing: -1px;
-    margin: 1em;
-    height: 1.5em;
-    border: none;
-  }
-  input[type="text"]:focus {
-    outline: none;
-  }
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-self: flex-start;
+		align-self: flex-start;
+		margin: none;
+		padding: none;
+	}
+
+	input[type="text"] {
+		font-family: "Cutive Mono", monospace;
+		letter-spacing: -1px;
+		margin: 1em;
+		height: 1.5em;
+		border: none;
+	}
+	input[type="text"]:focus {
+		outline: none;
+	}
 </style>
 
 
 <!-- pseudoHTML -------------------------------------------------------- -->
+
 <div class="container">
-
-	<nav>
-		<div class="navbutton">
-		<IconButton iconUrl="./assets/icons/save.png"/>
-		</div>
-		<div class="navbutton">
-		<IconButton iconUrl="./assets/icons/load.png"/>
-		</div>
-	</nav>
-
+	
+	<header>
+		<h1>{title}</h1>
+	</header>
 	<main>
-		<header>
-			<h1>{title}</h1>
-		</header>
+
+		<nav>
+			<IconButton iconUrl="./assets/icons/load.png"/>
+		</nav>
 
 		<div class="playground">
 			<PictureView />
 			<SurfaceView />
 			<div class="gcode">
 				<label for="title">title:</label>
-				<input type="text" bind:value={title} />
+				<input type="text" name="title" bind:value={title} />
+				<IconButton iconUrl="./assets/icons/save.png"/>	
 			</div>
 		</div>
 
-		<footer>
-			<p style="color: #bbb">©<a style="color: #bbb" href="https://www.edouardmortec.com.com">mortec</a> lockdown 2020</p>
-		</footer>
 	</main>
+	<footer>
+		<p style="color: #bbb">©<a style="color: #bbb" href="https://www.edouardmortec.com.com">mortec</a> lockdown 2020</p>
+	</footer>
 </div>
