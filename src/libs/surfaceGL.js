3e7a356a9a4b86db6f07a97e3c09d188fe6e617b
Mortec <contact@edouardmortec.com>
2020-11-27 18:00:38 +0100

    surfaceGL object DRAFT

--- src/libs/surfaceGL.js
+++ src/libs/surfaceGL.js
@@ -11,15 +11,43 @@ const vert=`
 precision mediump float;
 
 attribute vec2 a_position;
-varying vec2 v_texcoord;
 
 void main() {
-    v_texcoord = 1. - a_position;
-    gl_Position = vec4(1.0 - 2.0 * a_position, 0, 1);
+    gl_Position = vec4(a_position, 0.0, 1.0);
 }
 `
 
-const frag=``
+const frag=`
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 u_resolution;
+uniform sampler2D u_texture;
+uniform float u_scale;
+uniform float u_f;
+uniform float u_a;
+
+
+void compute ( vec2 positions, sampler2D texture, float f, float a, float scale ){
+
+    float x = positions.x;
+    float y = posittions.y;
+    float i = y * u_resolution.x + x;
+    float l = ( texture.r + texture.g + texture.b ) / 3.0;
+    float resX = u_resolution.x;
+    float resY = u_resolution.y;
+    
+    float z = sin( i/(resX * resY) * PI * ( l * resX/2 ) ) * a;
+
+    return vec4( x * scale, y * scale, l, z);
+}
+
+void main() {
+    vec2 positions = gl_FragCoord.xy/u_resolution;
+	gl_FragColor = compute( positions, u_texture, u_f, u_a);
+}
+`
 
 const getTexture = (id) => {
     const context2D = document.getElementById( id ).getContext('2d')
@@ -39,12 +67,11 @@ const compute =  regl({
     },
 
     attributes: {
-        a_position : new Float32Array([ -2, 0,   0, -2,   2, 2]),
+        a_position : new Float32Array([-1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0])
     },
 
     uniforms : {
-        u_resX: $surfaceStore.resX,
-        u_resY: $surfaceStore.resY,
+        u_resolution: new Float32Array([$surfaceStore.resX,$surfaceStore.resY]),
         u_texture: ()=>{ getTexture( 'pictureCanvas' )},
         u_scale: $surfaceStore.scale,
         u_f: $surfaceStore.f,
